3650e0c18b27af1ad4f5a705ad21c4eeab6db4b7
OrangeAFK <onepluslion@gmail.com>
2025-10-13 11:24:02 -0500

    created empty controller files

---
+++ project_1.srcs/sources_1/new/moore.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/13/2025 11:22:16 AM
+// Design Name: 
+// Module Name: moore
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module moore(
+
+    );
+endmodule
